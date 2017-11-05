3f89014b5e02ea5c4d6c7943ae9d76abf4f74518
maorshutman <maor@guardian-optech.com>
2017-11-05 19:16:36 +0200

    Deleted old project.

--- tm4c123g-flight-controller/Debug/buffer.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-buffer.obj: ../buffer.c
-buffer.obj: /Applications/ti/ccsv7/tools/compiler/arm_5.1.14/include/stdint.h
-buffer.obj: ../buffer.h
-
-../buffer.c: 
-/Applications/ti/ccsv7/tools/compiler/arm_5.1.14/include/stdint.h: 
-../buffer.h: 
--- tm4c123g-flight-controller/buffer.h
+++
@@ -1,7 +0,0 @@
-
-#define DATA_LENGTH 14
-
-extern char buff[DATA_LENGTH];
-extern uint8_t index;
-
-void WriteByteToBuffer(char ch);

6565c7d2bb6bf0450af150e1f47aa5eef7592c01
maorshutman <maorus12@gmail.com>
2017-11-18 22:52:00 +0200

    Deleting an old testing project.

--- compdcm_mpu9150/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/Users/maorshutman/ti/TivaWare_C_Series-2.1.3.156/driverlib/ccs/Debug/driverlib.lib" -l"/Users/maorshutman/ti/TivaWare_C_Series-2.1.3.156/sensorlib/ccs/Debug/sensorlib.lib"
-
--- flight_controller/Debug/buffer.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-buffer.obj: ../buffer.c
-buffer.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h
-buffer.obj: ../buffer.h
-
-../buffer.c: 
-/Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
-../buffer.h: 
